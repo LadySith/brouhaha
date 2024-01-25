ed018519bac6e2b8f65024dc951fd61972cd61fd
Sithe Ncube <me.sithe.ncube@gmail.com>
2024-01-25 18:25:38 +0200

    Created Environment with a lot of stuff inside

---
+++ Brouhaha/Assets/Scripts/NPCFollow.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCFollow : MonoBehaviour
+{
+    public GameObject Player;
+    public Animator animator;
+    //public GameObject NPC;
+    public float TargetDisctance;
+    public float AllowedDistance;
+    public float FollowSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Player.transform);
+        TargetDisctance = Vector3.Distance(Player.transform.position, transform.position);
+        if (TargetDisctance >= AllowedDistance)
+        {
+            
+            //change to running animation
+            transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, FollowSpeed * Time.deltaTime);
+        }
+        else
+        {
+            
+            //change to idle animation
+        }
+    }
+}
