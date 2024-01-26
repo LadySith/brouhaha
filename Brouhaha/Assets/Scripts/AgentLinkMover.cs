ff3a0d3859099d0beba8b4c032314bf65af826f6
Sithe Ncube <me.sithe.ncube@gmail.com>
2024-01-26 14:24:45 +0200

    Added enemy animations and audience panels

--- Brouhaha/Assets/Scripts/AgentLinkMover.cs
+++ Brouhaha/Assets/Scripts/AgentLinkMover.cs
@@ -15,6 +15,9 @@ public class AgentLinkMover : MonoBehaviour
 {
     public OffMeshLinkMoveMethod m_Method = OffMeshLinkMoveMethod.Parabola;
     public AnimationCurve m_Curve = new AnimationCurve();
+    public delegate void LinkEvent();
+    public LinkEvent OnLinkStart;
+    public LinkEvent OnLinkEnd;
 
     IEnumerator Start()
     {
@@ -22,16 +25,26 @@ public class AgentLinkMover : MonoBehaviour
         agent.autoTraverseOffMeshLink = false;
         while (true)
         {
+            
             if (agent.isOnOffMeshLink)
             {
+                OnLinkStart?.Invoke();
                 if (m_Method == OffMeshLinkMoveMethod.NormalSpeed)
+                {
                     yield return StartCoroutine(NormalSpeed(agent));
+                }
                 else if (m_Method == OffMeshLinkMoveMethod.Parabola)
-                    yield return StartCoroutine(Parabola(agent, 2.0f, 0.5f));
+                {
+                    yield return StartCoroutine(Parabola(agent, 4.0f, 0.5f));
+                }
                 else if (m_Method == OffMeshLinkMoveMethod.Curve)
+                {
                     yield return StartCoroutine(Curve(agent, 0.5f));
+                }
                 agent.CompleteOffMeshLink();
+                OnLinkEnd?.Invoke();
             }
+
             yield return null;
         }
     }
---
+++ Brouhaha/Assets/Scripts/Enemy/EnemyFollow.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent), typeof(AgentLinkMover))]
+public class EnemyFollow : MonoBehaviour
+{
+    public NavMeshAgent enemy;
+    public float UpdateRate = 0.1f;
+    public float attackDistance = 3f;
+    public Transform player;
+    public Animator animator;
+    private AgentLinkMover linkMover;
+
+    private Coroutine FollowCoroutine;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        enemy = GetComponent<NavMeshAgent>();
+        linkMover = GetComponent<AgentLinkMover>();
+
+        linkMover.OnLinkStart += HandleLinkStart;
+        linkMover.OnLinkEnd += HandleLinkEnd;
+    }
+
+    public void StartChasing()
+    {
+        if (FollowCoroutine == null)
+        {
+            FollowCoroutine = StartCoroutine(FollowTarget());
+        }
+        else
+        {
+            Debug.LogWarning("Called StartChasing on Enemy that is already chasing! This is likely a bug in some calling class!");
+        }
+    }
+
+    private IEnumerator FollowTarget()
+    {
+        WaitForSeconds Wait = new WaitForSeconds(UpdateRate);
+
+        while (gameObject.activeSelf)
+        {
+            enemy.SetDestination(player.position);
+            yield return Wait;
+        }
+    }
+
+    private void HandleLinkStart()
+    {
+        animator.SetTrigger("Jump");
+    }
+
+    private void HandleLinkEnd()
+    {
+        animator.SetTrigger("Landed");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemy.SetDestination(player.position);
+        if (Vector3.Magnitude(enemy.velocity) > 0.1f)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        if (enemy.remainingDistance < attackDistance)
+        {
+            animator.SetTrigger("Attack");
+        }
+    }
+}
